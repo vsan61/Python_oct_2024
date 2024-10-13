20c393b972281f705eecd86739c2d46d52bc8496
vsan61 <70624231+vsan61@users.noreply.github.com>
2024-10-13 08:35:17 +0530

    Add files via upload

---
+++ Date_type_conv.py
@@ -0,0 +1,64 @@
+#int to float
+a = 10
+b = float(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#int to str
+a = 20
+b = str(a)
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#int to bool
+a = 50
+b = bool(a)
+print(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#str to int
+a = '10'
+b = int(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#str to bool
+a = '10'
+b = bool(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#str to float
+a = '10'
+b = float(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#float to int
+a = 10.00
+b = int(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#float to bool
+a = 50.02
+b = bool(a)
+print(type(a))
+print(b)
+print(type(b))
+
+#float to str
+a = 100.6666
+b = str(a)
+print(type(a))
+print(b)
+print(type(b))
