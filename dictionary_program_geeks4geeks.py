4a93b8957ca56edab8ee0046cb1e9b2dcd810d3c
vsan61 <70624231+vsan61@users.noreply.github.com>
2024-11-10 12:50:32 +0530

    Update dictionary_program_geeks4geeks.py

--- dictionary_program_geeks4geeks.py
+++ dictionary_program_geeks4geeks.py
@@ -1,3 +1,30 @@
 #Python program to find the sum of all items in a dictionary
 """a={'a':1,'b':2,'c':3}
 print(sum(a.values()))"""
+#Python | Ways to remove a key from dictionary
+"""a={'a':1,'b':2,'c':3}
+#del a['b']
+a.pop('a')
+print(a)"""
+#Python | Merging two Dictionaries
+"""a={'a':1,'b':2,'c':3}
+b={"name":"rrrr", "age":13, "station":"marathalli"}
+a.update(b)
+print(a)"""
+#Python – Convert key-values list to flat dictionary
+"""test_dict = {'month' : [1, 2, 3],'name' : ['Jan', 'Feb', 'March']}
+res=dict(zip(test_dict['month'],test_dict['name']))
+print(res)"""
+#Python – Convert list to dictionary
+"""l1=[1,2,3,4]
+l2=['san','pal','jagan','kris']
+out=dict(zip(l1,l2))
+print(out)"""
+#Python – Inserting elements in dictionary at the beginning
+"""d1={'a':1,'b':2,'c':3}
+d1.update({'d':4})
+print(d1)"""
+#Python – Append Dictionary Keys and Values ( In order ) in dictionary
+"""test_dict = {"Gfg" : 1, "is" :  3, "Best" : 2}
+print(list(test_dict.keys())+list(test_dict.values()))"""
+
